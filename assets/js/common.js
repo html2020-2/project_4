0a743f16f2bf64f9e87cff9210198d542b370e1a
djyp1226 <72421988+djyp1226@users.noreply.github.com>
2021-01-07 16:09:25 +0900

    210107 first
    index

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,28 +1,28 @@
 $(document).ready(function () {
     // 네비게이션
 
-    var _dep1 = $('#gnb > ul');
-    var _first = _dep1.find('.first');
-    var _last = _dep1.find('.last');
+    var _dep1 = $('#gnb ul ul');
+    var _first = _dep1.find('[data-link="first"]');
+    var _last = _dep1.find('[data-link="last"]');
 
-    $('#gnb > btn_box').on('click', function () {
+    $('#nav_box button').on('click', function () {
           $(this).parent().toggleClass('on');
           if ($(this).parent().hasClass('on')) { //열려진 경우라면
             _dep1.stop().animate({
               right: '-100%'
-            }, 300, function () {
+            }, 1000, function () {
               $(this).parent().css({
                 visibility: 'hidden'
               });
             });
-            $(this).removeClass('on').text('MENU');
+            $(this).removeClass('on').text('EXIT');
           } else { //닫겨진 경우라면
-            $(this).addClass('on').text('EXIT');
+            $(this).addClass('on').text('MENU');
             _dep1.css({
               visibility: 'visible'
             }).stop().animate({
-              left: 0
-            }, 300, function () {
+              right: 0
+            },1000, function () {
               _first.focus(); //대상 엘리먼트에 포커스를 강제로 이동
             });
           }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,65 @@
 $(document).ready(function () {
-
-    $('#cnt1 .bgm .bgm_play').on('click', function () {
-        var audio = new Audio('../music/HansZimmer_FirstStep.ogg');
-
-        audio.src = '../music/HansZimmer_FirstStep.ogg';
-        audio.loop = true; // 반복재생하지 않음
-        audio.volume = 0.5; // 음량 설정
-        audio.play(); // sound1.mp3 재생
+    //인트로 화면 먼저
+    $('#intro').on('click', function () {
     });
-        // 인트로
 
-        var _typing = $('#typing p');
+    // 인트로
+    var _typing = $('#typing p');
 
-        //1) #typing 요소의 텍스트 가져오기
-        var text = _typing.text();
-        //console.log(text); //var=space;
-        //2) #typing 내부 제거
-        _typing.html('');
-        //3) #typing 내부 문자를 한 글자씩 잘라 배열에 저장
-        var chars = text.split('');
-        //console.log(chars); //['v', 'a', 'r', '=', 's', 'p', 'a', 'c', 'e', ';']
-        //4) 배열 chars의 각 문자들을 span 요소를 생성해 #typing에 추가
-        //$.each()메서드 => $.each(배열명, function(index번호, value값) {});
-        $.each( chars, function( index, value ) {
-            //console.log( index + ': ' + value );
-            if (index <= 2) {
-                _typing.append($('<span class="color"></span>').text(value));
-            } else {
-                _typing.append($('<span></span>').text(value));
+    //1) #typing 요소의 텍스트 가져오기
+    var text = _typing.text();
+    //console.log(text); //var=space;
+    //2) #typing 내부 제거
+    _typing.html('');
+    //3) #typing 내부 문자를 한 글자씩 잘라 배열에 저장
+    var chars = text.split('');
+    //console.log(chars); //['v', 'a', 'r', '=', 's', 'p', 'a', 'c', 'e', ';']
+    //4) 배열 chars의 각 문자들을 span 요소를 생성해 #typing에 추가
+    //$.each()메서드 => $.each(배열명, function(index번호, value값) {});
+    $.each( chars, function( index, value ) {
+        //console.log( index + ': ' + value );
+        if (index <= 2) {
+            _typing.append($('<span class="color"></span>').text(value));
+        } else {
+            _typing.append($('<span></span>').text(value));
+        }
+    });
+    //5) 글자를 표시하기 전의 지연 시간(ms)
+    var delayStart = 50;
+    //6) 타이핑 속도(ms)
+    var speed = 150;
+    //7) 글자들을 보이지 않게 설정한 다음 한 글자씩 화면에 표시
+    _typing.children().hide().each(function (index) {
+        var delay = delayStart + speed * index;
+        $(this).delay(delay).show(10);
+    });
+
+    var $caret = $('<p></p>').attr('id', 'caret').css({
+        width: '0.4em',
+    }).appendTo(_typing);
+    _typing.children(':not(#caret)').hide().each(function (index) {
+        var delay = delayStart + speed * index;
+        $(this).delay(delay).show(10);
+    });
+
+    //메인 음악
+
+        $('#cnt1 .bgm button').on('click', function () {
+            $(this).toggleClass('music');
+            
+            if ($(this).hasClass('music')) {
+                var audio = new Audio('assets/music/HansZimmer_FirstStep.ogg');
+                audio.loop = true; // 반복재생
+                audio.volume = 0.5; // 음량 설정
+                audio.play(); // 재생
+            } 
+            else if (!$(this).hasClass('music')) {
+                audio.pause();
             }
-        });
-        //5) 글자를 표시하기 전의 지연 시간(ms)
-        var delayStart = 50;
-        //6) 타이핑 속도(ms)
-        var speed = 150;
-        //7) 글자들을 보이지 않게 설정한 다음 한 글자씩 화면에 표시
-        _typing.children().hide().each(function (index) {
-            var delay = delayStart + speed * index;
-            $(this).delay(delay).show(10);
-        });
-
-        var $caret = $('<p></p>').attr('id', 'caret').css({
-            width: '0.4em',
-        }).appendTo(_typing);
-        _typing.children(':not(#caret)').hide().each(function (index) {
-            var delay = delayStart + speed * index;
-            $(this).delay(delay).show(10);
+            else {
+                // 멈추기가 안된다
+                e.preventDefault();
+            }
+            console.log($(this));
         });
 });
